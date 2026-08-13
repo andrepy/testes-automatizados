957b85ae1b6467e39161be0e6b7c27d111488f95
andrepy <andrep358windworld@gmail.com>
2026-08-13 13:36:40 -0300

    mais cobertura de testes

--- src/components/Button.test.jsx
+++ src/components/Button.test.jsx
@@ -6,3 +6,10 @@ test("renderiza o texto do botao", () => {
 
     expect(screen.getByText("Salvar")).toBeInTheDocument()
 })
+
+test('não quebra quando texto é vazio', () => {
+    const { rerender } = render(<Botao texto="" />)
+    expect(screen.getByRole("button")).toHaveTextContent('')
+    rerender(<Botao/>)
+    expect(screen.getByRole("button")).toBeInTheDocument()
+})
